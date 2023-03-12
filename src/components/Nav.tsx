bf3955675e82f0975b3f93690d8e48a665b57a5d
Emigdion13 <jemigdion@gmail.com>
2023-03-12 01:29:13 -0600

    Fixing Eslint errors

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { Link } from "react-router-dom";
 import { User } from "../models";
 
@@ -10,22 +9,22 @@ const Nav = (props: { user: User | null }) => {
         <nav className="bg-dark navbar navbar-dark navbar-expand-lg pb-3 position-relative pt-3 shadow-none" data-pg-collapsed>
             <div className="bg-secondary col-lg-8 end-0 h-100 position-absolute top-0">
             </div>
-            <div className="container position-relative"> <a className="fw-bold navbar-brand text-uppercase" href="">Logo</a>
+            <div className="container position-relative"> <a className="fw-bold navbar-brand text-uppercase" href="#top">Logo</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown-2" aria-controls="navbarNavDropdown-2" aria-expanded="false" aria-label="Toggle navigation"> <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse " id="navbarNavDropdown-2">
                     <ul className="ms-auto navbar-nav">
-                        <li className="nav-item"> <a className="active nav-link pb-lg-3 pe-lg-4 ps-lg-4 pt-lg-3" aria-current="page" href="">Home</a>
+                        <li className="nav-item"> <a className="active nav-link pb-lg-3 pe-lg-4 ps-lg-4 pt-lg-3" aria-current="page" href="#top">Home</a>
                         </li>
-                        <li className="nav-item"> <a className="nav-link pb-lg-3 pe-lg-4 ps-lg-4 pt-lg-3" href="" target="_pg_blank">About</a>
+                        <li className="nav-item"> <a className="nav-link pb-lg-3 pe-lg-4 ps-lg-4 pt-lg-3" href="#top">About</a>
                         </li>
-                        <li className="nav-item"> <a className="nav-link pb-lg-3 pe-lg-4 ps-lg-4 pt-lg-3" href="">Features</a>
+                        <li className="nav-item"> <a className="nav-link pb-lg-3 pe-lg-4 ps-lg-4 pt-lg-3" href="#top">Features</a>
                         </li>
-                        <li className="nav-item"> <a className="nav-link pb-lg-3 pe-lg-4 ps-lg-4 pt-lg-3" href="">Services</a>
+                        <li className="nav-item"> <a className="nav-link pb-lg-3 pe-lg-4 ps-lg-4 pt-lg-3" href="#top">Services</a>
                         </li>
-                        <li className="nav-item"> <a className="nav-link pb-lg-3 pe-lg-4 ps-lg-4 pt-lg-3" href="">Blog</a>
+                        <li className="nav-item"> <a className="nav-link pb-lg-3 pe-lg-4 ps-lg-4 pt-lg-3" href="#top">Blog</a>
                         </li>
-                        <li className="nav-item"> <a className="nav-link pb-lg-3 pe-lg-4 ps-lg-4 pt-lg-3" href="">Contact</a>
+                        <li className="nav-item"> <a className="nav-link pb-lg-3 pe-lg-4 ps-lg-4 pt-lg-3" href="#top">Contact</a>
                         </li>
                         <li className="nav-item"><Link to={'/profile'} className="nav-link pb-lg-3 pe-lg-4 ps-lg-4 pt-lg-3"> {props.user?.first_name} {props.user?.last_name} </Link>
                         </li>
